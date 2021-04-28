e012fe0bb668e91ad463452f00f1d2293c079d64
corian-youdale <corian.youdale@hotmail.com>
2021-04-28 14:35:01 +1200

    no more errors on master

--- client/actions/youtube.js
+++ client/actions/youtube.js
@@ -1,10 +1,3 @@
-import { google } from 'googleapis'
-
-const oauth2Client = new google.auth.OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-)
-
 export const consoleMe = () => {
 
     console.log("google")
