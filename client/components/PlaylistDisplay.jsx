4ec52035bf6d9fb946061b80677cfe538c2a45db
corian-youdale <corian.youdale@hotmail.com>
2021-04-29 11:59:57 +1200

    Added PlaylistDisplay function to show thumbnails

--- client/components/PlaylistDisplay.jsx
+++ client/components/PlaylistDisplay.jsx
@@ -8,12 +8,14 @@ const PlaylistDisplay = () => {
     const data = getPlaylist()
     data.then((data) => {
      return setVideoArray(returnTitleUrlData(data))
+    //  return setVideoArray(returnVideoThumbnail(data)) Gets Small Thumbnail
     })
   }, [])
 
   return (
     <ul>
       {videoArray.map((video) => <li key={video.title}><a href={video.url}>{video.title}</a></li>)}
+      {/* {videoArray.map((video) => <li key={video.title}><img src={video.thumbnailURL}></img></li>)} GETS THUMBNAIL */}
     </ul>
   )
 }
@@ -22,6 +24,17 @@ const returnTitleUrlData = (data) => {
   return data.items.map((song) => {
     return {
       title: song.snippet.title,
+      url: `https://www.youtube.com/watch?v=${song.snippet.resourceId.videoId}`
+    }
+  })
+}
+
+// displays small thumbnail for each video
+const returnVideoThumbnail = (data) => {
+  return data.items.map((song) => {
+    console.log(song)
+    return {
+      thumbnailURL: song.snippet.thumbnails.default.url,
       url: `https://www.youtube.com/watch?v=${song.snippet.resourceId.videoId}`
     }
   })
